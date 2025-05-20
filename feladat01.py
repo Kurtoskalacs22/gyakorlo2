142d0d2b5edb885f72875ab9aa69cce795e9e00b
Petrik Diak <diak@petrik.hu>
2025-05-20 11:58:18 +0200

    first commit

--- feladat01.py
+++ feladat01.py
@@ -1,1 +1,10 @@
-print('hello')
+nev = input("Kérem adja meg Anna mire gyűjti a pénzt: ")
+szam = int(input("Kérem adja meg hány kutyát sétáltat: "))
+
+ora = (szam * 20)//60
+perc =(szam * 20)%60
+bevetel = 700*szam
+if bevetel >= 5000:
+   print(f"Anna {szam} kutyát sétáltatott {ora}:{perc} alatt, ezért {bevetel} Ft-ot kapott. Ez elég a {nev} árához.")
+else:
+    print(f"Anna {szam} kutyát sétáltatott {ora}:{perc} alatt, ezért {bevetel} Ft-ot kapott. Ez nem elég a {nev} árához.")
